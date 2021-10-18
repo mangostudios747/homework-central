05256c406e5ecf5d4617dae58f1cbb7606b74056
saumyasinghal747 <saumyasmathtutoring@gmail.com>
2021-10-18 04:16:45 +0000

    add lifelines

---
+++ src/components/Barcode.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h5>Barcode</h5>
+    <b-col>
+      <vue-barcode
+        format="CODE39"
+        class="mx-auto d-flex"
+        width="3"
+        :value="idNumber || '95044523'"
+      ></vue-barcode>
+      <b-form-input
+        class="d-block mt-1 text-center"
+        trim
+        v-model="idNumber"
+      ></b-form-input>
+    </b-col>
+  </div>
+</template>
+
+<script>
+import VueBarcode from "vue-barcode";
+export default {
+  data: () => ({
+    idNumber: "95000000",
+  }),
+  components: {
+    VueBarcode,
+  },
+  mounted() {
+    this.idNumber = this.$state.settings.idNumber || "95000000";
+  },
+  watch: {
+    async idNumber(val) {
+      // set the idNumber in settings to this value
+      this.$store.commit("settings/setIdNumber", this.idNumber);
+      //alert(val)
+      //alert(JSON.parse(localStorage.getItem('hcSettings')).idNumber)
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Lifelines.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    style="
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      gap: 10px;
+      grid-auto-rows: minmax(100px, auto);
+    "
+  >
+    <div style="" :key="c.phoneNumber" v-for="c of lifelines">
+      <h5>{{ c.name }}</h5>
+      <a :href="'tel:' + c.phoneNumber">{{ c.phoneNumber }}</a>
+      <p>{{ c.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import lifelines from "@/plugins/crisislifelines.json";
+export default {
+  data: () => ({
+    lifelines,
+  }),
+};
+</script>
+
+<style></style>
